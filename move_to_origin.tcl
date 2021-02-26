51f62b0bac788b6a8ae44e486f34b5ee9390b3da
Myungjin Lee <myungjil@usc.edu>
2021-02-26 00:29:48 -0500

    Delete move_to_origin.tcl

--- move_to_origin.tcl
+++
@@ -1,8 +0,0 @@
-set inpdb [lindex $argv 0]
-set outpdb [lindex $argv 1]
-mol load pdb $inpdb
-move_to_pro_origin [atomselect top "protein"] [atomselect top "all"]
-set sel [atomselect top "all"]
-$sel writepdb $outpdb
-mol delete all
-exit
